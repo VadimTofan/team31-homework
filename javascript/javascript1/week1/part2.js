c42a08d891a9fc0d3a4dfbeb373a66477a4b6928
Vadim Tofan <vad.tofan@gmail.com>
2025-02-02 15:56:25 +0100

    Class excercises 3 & 4  with Bjorn

--- javascript/javascript1/week1/part2.js
+++ javascript/javascript1/week1/part2.js
@@ -3,8 +3,8 @@
 console.log("===== PART 2 =====");
 
 
-const owner = 'Carlo';
-const restaurantName = 'Bella Napoli';
+let owner = 'Carlo';
+let restaurantName = 'Bella Napoli';
 const specialOffer = "Pesto Pizza";
 const description = 'Experience the true Italian cuisine!';
 const address = 'Nom Nom Street 3, 2300 Copenhagen';
@@ -26,8 +26,6 @@ const pizzaBbqChicken = 'BBQ Chicken Pizza';
 const pizzaBbqChickenPrice = 130;
 const pizzaWhite = 'White Pizza';
 const pizzaWhitePrice = 110;
-const pizzaMeatLover = "Meat Lover's Pizza";
-const pizzaMeatLoverPrice = 135;
 console.log('Welcome to', restaurantName);
 console.log(description);
 console.log(ourMenu);
@@ -36,4 +34,3 @@ console.log('#2', pizzaPesto, '-', pizzaPestoPrice, 'DKK');
 console.log('#3', pizzaPepperoni, '-', pizzaPepperoniPrice, 'DKK');
 console.log('#4', pizzaBbqChicken, '-', pizzaBbqChickenPrice, 'DKK');
 console.log('#5', pizzaWhite, '-', pizzaWhitePrice, 'DKK');
-console.log('#6', pizzaMeatLover, '-', pizzaMeatLoverPrice, 'DKK');
---
+++ javascript/javascript1/week1/part4.js
@@ -0,0 +1,31 @@
+
+console.log("===== PART 4 =====");
+
+
+let order2 = [pizzas[1], pizzas[2], pizzas[2]];
+let totalPrice2 = pizzaPrices[1] + pizzaPrices[2] + pizzaPrices[2];
+let pizzaDiscount = pizzaPrices[1]*0.2;
+console.log("Order: ", order2);
+console.log("Total price: ", totalPrice2, "DKK");
+console.log("Pizza discount: ", pizzaDiscount, "DKK");
+const addPlasticBag = true;
+if (addPlasticBag){
+    let plasticBagPrice = 5;
+    console.log ("Added plastic bag: 5 DKK");
+} else {
+    let plasticBagPrice = 0;
+    console.log ("No plastic bag added");
+}
+
+let finalPrice1 = totalPrice2 + addPlasticBag - pizzaDiscount;
+const addVat = 0.25;
+console.log("VAT (25%): ", addVat*finalPrice1, "DKK");
+let finalPrice2 = finalPrice1 + addVat*finalPrice1 + plasticBagPrice;
+console.log("Final price: ", finalPrice2, "DKK");   
+
+
+
+
+
+
+
