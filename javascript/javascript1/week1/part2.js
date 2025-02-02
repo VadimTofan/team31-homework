00205146f1800e9845b6465e8108a4179b4d9a8b
Vadim Tofan <vad.tofan@gmail.com>
2025-02-02 13:47:38 +0100

    Class Excersises with JavaScript | Week - 1

---
+++ javascript/javascript1/week1/part2.js
@@ -0,0 +1,39 @@
+
+
+console.log("===== PART 2 =====");
+
+// Info about us
+const owner = 'Carlo';
+const restaurantName = 'Bella Napoli';
+const specialOffer = "Pesto Pizza";
+const description = 'Experience the true Italian cuisine!';
+const address = 'Nom Nom Street 3, 2300 Copenhagen';
+let openTime = '12:00';
+let closingTime = '21:00';
+const phone = '45 12 34 56 78';
+const email = 'bella-napoli@hyf.com';
+const isVegan = true;
+const isSmokingAllowed = false;
+const isDogFriendly = false;
+const ourMenu = "Our Menu";
+const pizzaMargherita = 'Margherita Pizza';
+const pizzaMargheritaPrice = 90;
+const pizzaPesto = 'Pesto Pizza';
+const pizzaPestoPrice = 120;
+const pizzaPepperoni = 'Pepperoni Pizza';
+const pizzaPepperoniPrice = 120;
+const pizzaBbqChicken = 'BBQ Chicken Pizza';
+const pizzaBbqChickenPrice = 130;
+const pizzaWhite = 'White Pizza';
+const pizzaWhitePrice = 110;
+const pizzaMeatLover = "Meat Lover's Pizza";
+const pizzaMeatLoverPrice = 135;
+console.log('Welcome to', restaurantName);
+console.log(description);
+console.log(ourMenu);
+console.log('#1', pizzaMargherita, '-', pizzaMargheritaPrice, 'DKK');
+console.log('#2', pizzaPesto, '-', pizzaPestoPrice, 'DKK');
+console.log('#3', pizzaPepperoni, '-', pizzaPepperoniPrice, 'DKK');
+console.log('#4', pizzaBbqChicken, '-', pizzaBbqChickenPrice, 'DKK');
+console.log('#5', pizzaWhite, '-', pizzaWhitePrice, 'DKK');
+console.log('#6', pizzaMeatLover, '-', pizzaMeatLoverPrice, 'DKK');
