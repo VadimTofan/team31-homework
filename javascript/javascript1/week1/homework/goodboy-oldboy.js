d55af84e974fcb9e51b75545e25c55507f81d91e
Vadim Tofan <vad.tofan@gmail.com>
2025-02-08 16:27:49 +0100

    Changed Let to const in ez-namey.js | Changed goodboy-oldboy.js dog and human years to actually make sense | changed descriptions and usage of arguments in housey-pricey.js

--- javascript/javascript1/week1/homework/goodboy-oldboy.js
+++ javascript/javascript1/week1/homework/goodboy-oldboy.js
@@ -7,7 +7,7 @@ const dogYearFuture = 2027;
 const dogYear = dogYearFuture - dogYearOfBirth;
 const shouldShowResultInDogYears = true;
 if (shouldShowResultInDogYears) {
-    console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture + ".");
+    console.log("Your dog will be " + dogYear * 7 + " dog years old in " + dogYearFuture + ".");
 } else {
-    console.log("Your dog will be " + dogYear * 7 + " human years old in " + dogYearFuture + ".");
+    console.log("Your dog will be " + dogYear + " human years old in " + dogYearFuture + ".");
 }
