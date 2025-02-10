018cd6bf1f2669400f42941b246a94d9b16d86d6
Vadim Tofan <vad.tofan@gmail.com>
2025-02-10 02:18:58 +0100

    Add HTML and JavaScript files for Week 2 homework exercises

---
+++ javascript/javascript1/week2/Homework/script.js
@@ -0,0 +1,15 @@
+
+
+console.log("======= Homework Week 2 =======");
+console.log("======= Exercise 1 =======");
+
+function getFullname (firstname, surname){
+    return firstname + " " + surname;
+}
+
+console.log(getFullname("Albert", "Grimaldi"));
+const fullname1 = getFullname("Carl-Philip", "Bertil");
+console.log(fullname1);
+const fullname2 = getFullname("George-Alexander", "Louis");
+console.log(fullname2);
+
