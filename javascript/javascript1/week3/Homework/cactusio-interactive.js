a8504a7c3730bd3e9807a013c3cd591f3035d15f
Vadim Tofan <vad.tofan@gmail.com>
2025-02-19 15:05:05 +0100

    Changed activity tracking script to use plural 'activities' and improve function parameter naming for clarity

--- javascript/javascript1/week3/Homework/cactusio-interactive.js
+++ javascript/javascript1/week3/Homework/cactusio-interactive.js
@@ -1,7 +1,7 @@
 console.log("=========CactusIO Interactive=========");
 
 
-const activity = [{
+const activities = [{
     date: "13/02/2025",
     activity: "Facebook", //Had to add this manually, as inputs take today's date.
     duration: 35          //And i wanted to test if choosing only specific date would work.
@@ -9,12 +9,12 @@ const activity = [{
 const today = new Date();
 const todayDate = today.toLocaleDateString("en-GB")
 
-function addActivity(app, duration) {
-    if (typeof app === "string" && typeof duration === "number" && duration > 0) {
-        activity.push({ date: todayDate, activity: app, duration: duration });
-        console.log("Activity added:", activity);
+function addActivity(activity, duration) {
+    if (typeof activity === "string" && typeof duration === "number" && duration > 0) {
+        activities.push({ date: todayDate, activity: activity, duration: duration });
+        console.log("Activity added:", activities);
     } else {
-        console.warn("You have made a mistake in the input. Format: App -> 'string', Duration -> number > 0");
+        console.warn(`You have made a mistake in the input. Format: activity -> 'string', Duration -> number > 0`);
     }
 }
 
@@ -23,25 +23,25 @@ addActivity("Youtube", 1);
 addActivity(23, "Instagram");      
 addActivity("Candy Crush Saga", 555);  
 
-console.log(activity); 
+console.log(activities); 
 
 function showStatus() {
     let activityDuration = 0;
     let activitiesToday = 0;
 
-    for (let i = 0; i < activity.length; i++) {
-        activityDuration = activity[i].duration + activityDuration;
+    for (let i = 0; i < activities.length; i++) {
+        activityDuration = activities[i].duration + activityDuration;
     }
 
-    for (let i = 0; i < activity.length; i++) {
-        if (activity[i].date === todayDate) {
+    for (let i = 0; i < activities.length; i++) {
+        if (activities[i].date === todayDate) {
             activitiesToday = activitiesToday+1;
         }
     }
 
     if (activitiesToday  === 0) {
         console.log("You don't have any activities registered for today.")
-    } else if (activity.length === 0) {
+    } else if (activities.length === 0) {
         console.log("You don't have any registered activities.")
     } else {
         console.log(`You have added ${activitiesToday} activities. They amount to ${activityDuration} min. of usage`)
@@ -52,23 +52,23 @@ showStatus();
 
 
 function mostTimeSpent() {
-    let mostActivity = activity[0];
+    let mostUsedActivity = activities[0];
 
-    if (activity.length === 0) {
+    if (activities.length === 0) {
         console.log("You don't have any registered activities.");
         return;
     }
 
-    for (let i = 1; i < activity.length; i++) {
-        if (activity[i].duration > mostActivity.duration) {
-            mostActivity = activity[i];
+    for (let i = 1; i < activities.length; i++) {
+        if (activities[i].duration > mostUsedActivity.duration) {
+            mostUsedActivity = activities[i];
         }
     }
 
-    if (mostActivity.duration === 0) {
+    if (mostUsedActivity.duration === 0) {
         console.log("All your registered activities have a duration of 0 minutes.");
     } else {
-        console.log("The activity you spent the most time on is '" + mostActivity.activity + "' with a total duration of " + mostActivity.duration + " minutes.");
+        console.log("The activity you spent the most time on is '" + mostUsedActivity.activity + "' with a total duration of " + mostUsedActivity.duration + " minutes.");
     }
 }
 
