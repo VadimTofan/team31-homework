7797f3da9087ac9bdbc0598ae5d11e1735bbd76b
Vadim Tofan <vad.tofan@gmail.com>
2025-02-19 16:55:02 +0100

    Added a Job-Matching script-2 to check the desired equity or locations on the job advertisement

--- javascript/javascript1/week3/Homework/job-matching-2.js
+++ javascript/javascript1/week3/Homework/job-matching-2.js
@@ -2,15 +2,41 @@ console.log("=========Bonus Homework!=========");
 console.log("=========Job Matching-2!=========");
 
 
-function match(candidate, job) {
-  if (!candidate.minSalary || !job.maxSalary) {
-    return "The provided data was not complete.";
-  }
-  const wiggledSalary = candidate.minSalary - (candidate.minSalary * 0.1);
 
-  return job.maxSalary >= wiggledSalary;
+//This works on my computer perfectly, can't get it working on the codewars.
+
+let candidates = [{
+  desiresEquity: true, 
+  currentLocation: 'Dhaka',
+  desiredLocations: ['San Francisco', 'Copenhagen', 'Colorado'
+]}, {
+  desiresEquity: false, 
+  currentLocation: 'New York',
+  desiredLocations: ['Sao Paulo', 'Seattle', 'Texas']
+}];
+
+function match2(jobs, candidates) {
+  let results = [];
+
+  for (let i = 0; i < candidates.length; i++) {
+    const candidate = candidates[i];
+    const equityMatch = !candidate.desiresEquity || jobs.equityMax > 0;
+    let locationMatch = false;
+
+    for (let j = 0; j < jobs.locations.length; j++) {
+      if (candidate.currentLocation === jobs.locations[j] || candidate.desiredLocations.includes(jobs.locations[j])) {
+        locationMatch = true;
+        break;
+      }
+    }
+    results.push({candidate: i + 1, equityMatch: equityMatch, locationMatch: locationMatch })
+  }
+
+  return results
 }
 
-let candidate = { minSalary: 120000 };
-let job = { maxSalary: 140000 };
-console.log(match(candidate, job)); 
+const job1 = { equityMax: 0, locations: ['Los Angeles', 'New York'] }
+const job2 = { equityMax: 1.2, locations: ['New York', 'Kentucky'] }
+
+console.log(match2(job1, candidates))
+console.log(match2(job2, candidates))
--- javascript/javascript1/week3/Homework/job-matching.js
+++ javascript/javascript1/week3/Homework/job-matching.js
@@ -1,16 +1,16 @@
 console.log("=========Bonus Homework!=========");
-console.log("=========Job Matching!=========");
+console.log("=========Job Matching-1!=========");
 
 
-function match(candidate, job) {
-  if (!candidate.minSalary || !job.maxSalary) {
+function match(candidat, job) {
+  if (!candidat.minSalary || !job.maxSalary) {
     return "The provided data was not complete.";
   }
-  const wiggledSalary = candidate.minSalary - (candidate.minSalary * 0.1);
+  const wiggledSalary = candidat.minSalary - (candidat.minSalary * 0.1);
 
   return job.maxSalary >= wiggledSalary;
 }
 
-let candidate = { minSalary: 120000 };
+let candidat = { minSalary: 120000 };
 let job = { maxSalary: 140000 };
-console.log(match(candidate, job)); 
+console.log(match(candidat, job)); 
