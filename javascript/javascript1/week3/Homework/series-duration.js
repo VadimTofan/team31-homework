a51dd86c55797c2c335c9335b2f1bec72d1a1c29
Vadim Tofan <vad.tofan@gmail.com>
2025-02-19 15:17:43 +0100

    Changed series duration calculation for clarity and consistency in variable naming

--- javascript/javascript1/week3/Homework/series-duration.js
+++ javascript/javascript1/week3/Homework/series-duration.js
@@ -1,6 +1,6 @@
 console.log("=========Series Duration=========");
 
-const seriesDurations = [
+const series = [
   {
     title: "Game of Thrones",
     days: 3,
@@ -21,20 +21,18 @@ const seriesDurations = [
   },
 ];
 
-let totalViewTime = 0; 
-const ageInMinutes = 80 * 365 * 24 * 60;
+function seriesDuration() {  //function logOutSeriesText() didn't make much sense (as was written in homework)
+  let totalViewTime = 0; 
+  const ageInMinutes = 80 * 365 * 24 * 60;  
 
-function logOutSeriesText() {
-    for (i = 0; i < seriesDurations.length; i++){
-        const seriesTime = (seriesDurations[i].days * 24 * 60) + (seriesDurations[i].hours * 60) + seriesDurations[i].minutes;
-
-        let percentOfAge = 0;
-        percentOfAge = (seriesTime / ageInMinutes) * 100;
-        console.log(`${seriesDurations[i].title} took ${(percentOfAge).toFixed(3)}% of my life`);
-        totalViewTime = totalViewTime + seriesTime;
-    }   
+  for (i = 0; i < series.length; i++){
+    const seriesTime = (series[i].days * 24 * 60) + (series[i].hours * 60) + series[i].minutes;
+    const percentOfAge = (seriesTime / ageInMinutes) * 100;
+    console.log(`${series[i].title} took ${(percentOfAge).toFixed(3)}% of my life`);
+    totalViewTime = totalViewTime + seriesTime;
+  }   
     totalViewTime = (totalViewTime / ageInMinutes) * 100;
     console.log(`In total that took ${totalViewTime.toFixed(3)}% of my life`);
 }
 
-logOutSeriesText();
+seriesDuration();
