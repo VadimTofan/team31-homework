51e02866dd357d8141b72399466ac066e1648fd4
Vadim Tofan <vad.tofan@gmail.com>
2025-02-23 12:44:53 +0100

    Refactor variable declarations for clarity in Variables & Scope exercise

--- javascript/javascript1/week4/0.2 Variables & Scope.js
+++ javascript/javascript1/week4/0.2 Variables & Scope.js
@@ -1,15 +1,13 @@
 const name = "Alice";
 const age = 16;
 const country = "USA";
-let message = "";
-let status = "";
 
 if (age < 18) {
-  status = "minor";
+  let status = "minor";
 } else {
   status = "adult";
 }
 
-message = `${name} is a ${status} from ${country}.`;
+const message = `${name} is a ${status} from ${country}.`;
 
 console.log(message); // Alice is a minor from USA.
