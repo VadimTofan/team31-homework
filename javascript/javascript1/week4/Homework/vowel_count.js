63829e5970f718b4a92dc4f2192a9b3194234e6d
Vadim Tofan <vad.tofan@gmail.com>
2025-02-24 20:50:20 +0100

    Add vowel counting function to calculate and display the number of vowels in a string

--- javascript/javascript1/week4/Homework/vowel_count.js
+++ javascript/javascript1/week4/Homework/vowel_count.js
@@ -1,1 +1,26 @@
 console.log("===========Vowel Count===========");
+
+function getCount(str) {
+  let a = "a",
+    e = "e",
+    i = "i",
+    o = "o",
+    u = "u";
+  let count = 0;
+
+  for (let j = 0; j < str.length; j++) {
+    if (
+      str[j] === a ||
+      str[j] === e ||
+      str[j] === i ||
+      str[j] === o ||
+      str[j] === u
+    ) {
+      count++;
+    }
+  }
+
+  return `The amount of vowels is ${count};`;
+}
+
+console.log(getCount("abracadabra"));
