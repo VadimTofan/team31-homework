fd5a56a2009e154e8be3f6f2578541a1824e4b62
Vadim Tofan <vad.tofan@gmail.com>
2025-02-27 23:10:30 +0100

    Add imdbReview.js for movie rating filtering functionality

---
+++ javascript/javascript1/week4/Class Exercises/imdbReview.js
@@ -0,0 +1,23 @@
+console.log("=======1.2 IMDB Review=======");
+function filter(movie) {
+  let array = [];
+  for (let i = 0; i < movie.length; i++) {
+    if (movie[i].rating >= 8) {
+      array.push(movie[i].title);
+    }
+  }
+  return array;
+}
+
+let movieData = [
+  { title: "Interstellar", rating: 8.6 },
+  { title: "The Room", rating: 3.7 },
+  { title: "The Godfather", rating: 9.2 },
+  { title: "Cars", rating: 7.1 },
+];
+
+console.log(filter(movieData));
+
+// This function filters the array of ojbects (movieData)
+// and pushes the object names of movis with rating more than 8
+// into a new array called (array)
