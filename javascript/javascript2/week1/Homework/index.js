fe42ef7221ef8f3a8c5b3493e692a3b47716035b
Vadim Tofan <vad.tofan@gmail.com>
2025-03-03 15:04:59 +0100

    Add new card images in .webp format and remove outdated .jpg files for Homework project

--- javascript/javascript2/week1/Homework/index.js
+++ javascript/javascript2/week1/Homework/index.js
@@ -1,86 +1,65 @@
 const cardSelector = document.querySelectorAll("#card-one, #card-two, #card-three, #card-four, #card-five, #card-six, #card-seven, #card-eight, #card-nine, #card-ten, #card-eleven, #card-twelve");
 
 const cardsArray = [
-  { cardName: "Ace of Spades", cardUrl: "./cards/AS.jpg" },
-  { cardName: "Two of Spades", cardUrl: "./cards/2S.jpg" },
-  { cardName: "Three of Spades", cardUrl: "./cards/3S.jpg" },
-  { cardName: "Four of Spades", cardUrl: "./cards/4S.jpg" },
-  { cardName: "Five of Spades", cardUrl: "./cards/5S.jpg" },
-  { cardName: "Six of Spades", cardUrl: "./cards/6S.jpg" },
-  { cardName: "Seven of Spades", cardUrl: "./cards/7S.jpg" },
-  { cardName: "Eight of Spades", cardUrl: "./cards/8S.jpg" },
-  { cardName: "Nine of Spades", cardUrl: "./cards/9S.jpg" },
-  { cardName: "Ten of Spades", cardUrl: "./cards/10S.jpg" },
-  { cardName: "Jack of Spades", cardUrl: "./cards/JS.jpg" },
-  { cardName: "Queen of Spades", cardUrl: "./cards/QS.jpg" },
-  { cardName: "King of Spades", cardUrl: "./cards/KS.jpg" },
+  { cardName: "Ace of Spades", cardUrl: "./cards/AS.webp" },
+  { cardName: "Two of Spades", cardUrl: "./cards/2S.webp" },
+  { cardName: "Three of Spades", cardUrl: "./cards/3S.webp" },
+  { cardName: "Four of Spades", cardUrl: "./cards/4S.webp" },
+  { cardName: "Five of Spades", cardUrl: "./cards/5S.webp" },
+  { cardName: "Six of Spades", cardUrl: "./cards/6S.webp" },
+  { cardName: "Seven of Spades", cardUrl: "./cards/7S.webp" },
+  { cardName: "Eight of Spades", cardUrl: "./cards/8S.webp" },
+  { cardName: "Nine of Spades", cardUrl: "./cards/9S.webp" },
+  { cardName: "Ten of Spades", cardUrl: "./cards/10S.webp" },
+  { cardName: "Jack of Spades", cardUrl: "./cards/JS.webp" },
+  { cardName: "Queen of Spades", cardUrl: "./cards/QS.webp" },
+  { cardName: "King of Spades", cardUrl: "./cards/KS.webp" },
 
-  { cardName: "Ace of Hearts", cardUrl: "./cards/AH.jpg" },
-  { cardName: "Two of Hearts", cardUrl: "./cards/2H.jpg" },
-  { cardName: "Three of Hearts", cardUrl: "./cards/3H.jpg" },
-  { cardName: "Four of Hearts", cardUrl: "./cards/4H.jpg" },
-  { cardName: "Five of Hearts", cardUrl: "./cards/5H.jpg" },
-  { cardName: "Six of Hearts", cardUrl: "./cards/6H.jpg" },
-  { cardName: "Seven of Hearts", cardUrl: "./cards/7H.jpg" },
-  { cardName: "Eight of Hearts", cardUrl: "./cards/8H.jpg" },
-  { cardName: "Nine of Hearts", cardUrl: "./cards/9H.jpg" },
-  { cardName: "Ten of Hearts", cardUrl: "./cards/10H.jpg" },
-  { cardName: "Jack of Hearts", cardUrl: "./cards/JH.jpg" },
-  { cardName: "Queen of Hearts", cardUrl: "./cards/QH.jpg" },
-  { cardName: "King of Hearts", cardUrl: "./cards/KH.jpg" },
+  { cardName: "Ace of Hearts", cardUrl: "./cards/AH.webp" },
+  { cardName: "Two of Hearts", cardUrl: "./cards/2H.webp" },
+  { cardName: "Three of Hearts", cardUrl: "./cards/3H.webp" },
+  { cardName: "Four of Hearts", cardUrl: "./cards/4H.webp" },
+  { cardName: "Five of Hearts", cardUrl: "./cards/5H.webp" },
+  { cardName: "Six of Hearts", cardUrl: "./cards/6H.webp" },
+  { cardName: "Seven of Hearts", cardUrl: "./cards/7H.webp" },
+  { cardName: "Eight of Hearts", cardUrl: "./cards/8H.webp" },
+  { cardName: "Nine of Hearts", cardUrl: "./cards/9H.webp" },
+  { cardName: "Ten of Hearts", cardUrl: "./cards/10H.webp" },
+  { cardName: "Jack of Hearts", cardUrl: "./cards/JH.webp" },
+  { cardName: "Queen of Hearts", cardUrl: "./cards/QH.webp" },
+  { cardName: "King of Hearts", cardUrl: "./cards/KH.webp" },
 
-  { cardName: "Ace of Diamonds", cardUrl: "./cards/AD.jpg" },
-  { cardName: "Two of Diamonds", cardUrl: "./cards/2D.jpg" },
-  { cardName: "Three of Diamonds", cardUrl: "./cards/3D.jpg" },
-  { cardName: "Four of Diamonds", cardUrl: "./cards/4D.jpg" },
-  { cardName: "Five of Diamonds", cardUrl: "./cards/5D.jpg" },
-  { cardName: "Six of Diamonds", cardUrl: "./cards/6D.jpg" },
-  { cardName: "Seven of Diamonds", cardUrl: "./cards/7D.jpg" },
-  { cardName: "Eight of Diamonds", cardUrl: "./cards/8D.jpg" },
-  { cardName: "Nine of Diamonds", cardUrl: "./cards/9D.jpg" },
-  { cardName: "Ten of Diamonds", cardUrl: "./cards/10D.jpg" },
-  { cardName: "Jack of Diamonds", cardUrl: "./cards/JD.jpg" },
-  { cardName: "Queen of Diamonds", cardUrl: "./cards/QD.jpg" },
-  { cardName: "King of Diamonds", cardUrl: "./cards/KD.jpg" },
+  { cardName: "Ace of Diamonds", cardUrl: "./cards/AD.webp" },
+  { cardName: "Two of Diamonds", cardUrl: "./cards/2D.webp" },
+  { cardName: "Three of Diamonds", cardUrl: "./cards/3D.webp" },
+  { cardName: "Four of Diamonds", cardUrl: "./cards/4D.webp" },
+  { cardName: "Five of Diamonds", cardUrl: "./cards/5D.webp" },
+  { cardName: "Six of Diamonds", cardUrl: "./cards/6D.webp" },
+  { cardName: "Seven of Diamonds", cardUrl: "./cards/7D.webp" },
+  { cardName: "Eight of Diamonds", cardUrl: "./cards/8D.webp" },
+  { cardName: "Nine of Diamonds", cardUrl: "./cards/9D.webp" },
+  { cardName: "Ten of Diamonds", cardUrl: "./cards/10D.webp" },
+  { cardName: "Jack of Diamonds", cardUrl: "./cards/JD.webp" },
+  { cardName: "Queen of Diamonds", cardUrl: "./cards/QD.webp" },
+  { cardName: "King of Diamonds", cardUrl: "./cards/KD.webp" },
 
-  { cardName: "Ace of Clubs", cardUrl: "./cards/AC.jpg" },
-  { cardName: "Two of Clubs", cardUrl: "./cards/2C.jpg" },
-  { cardName: "Three of Clubs", cardUrl: "./cards/3C.jpg" },
-  { cardName: "Four of Clubs", cardUrl: "./cards/4C.jpg" },
-  { cardName: "Five of Clubs", cardUrl: "./cards/5C.jpg" },
-  { cardName: "Six of Clubs", cardUrl: "./cards/6C.jpg" },
-  { cardName: "Seven of Clubs", cardUrl: "./cards/7C.jpg" },
-  { cardName: "Eight of Clubs", cardUrl: "./cards/8C.jpg" },
-  { cardName: "Nine of Clubs", cardUrl: "./cards/9C.jpg" },
-  { cardName: "Ten of Clubs", cardUrl: "./cards/10C.jpg" },
-  { cardName: "Jack of Clubs", cardUrl: "./cards/JC.jpg" },
-  { cardName: "Queen of Clubs", cardUrl: "./cards/QC.jpg" },
-  { cardName: "King of Clubs", cardUrl: "./cards/KC.jpg" },
+  { cardName: "Ace of Clubs", cardUrl: "./cards/AC.webp" },
+  { cardName: "Two of Clubs", cardUrl: "./cards/2C.webp" },
+  { cardName: "Three of Clubs", cardUrl: "./cards/3C.webp" },
+  { cardName: "Four of Clubs", cardUrl: "./cards/4C.webp" },
+  { cardName: "Five of Clubs", cardUrl: "./cards/5C.webp" },
+  { cardName: "Six of Clubs", cardUrl: "./cards/6C.webp" },
+  { cardName: "Seven of Clubs", cardUrl: "./cards/7C.webp" },
+  { cardName: "Eight of Clubs", cardUrl: "./cards/8C.webp" },
+  { cardName: "Nine of Clubs", cardUrl: "./cards/9C.webp" },
+  { cardName: "Ten of Clubs", cardUrl: "./cards/10C.webp" },
+  { cardName: "Jack of Clubs", cardUrl: "./cards/JC.webp" },
+  { cardName: "Queen of Clubs", cardUrl: "./cards/QC.webp" },
+  { cardName: "King of Clubs", cardUrl: "./cards/KC.webp" },
 ];
 
-// for (let i = 1; i <= 12; i++) {
-//   let card = document.getElementById(`card-${i}`);
-//   console.log(card);
-// }
-
-const cardOne = document.getElementById("card-one");
-const cardTwo = document.getElementById("card-two");
-const cardThree = document.getElementById("card-three");
-const cardFour = document.getElementById("card-four");
-const cardFive = document.getElementById("card-five");
-const cardSix = document.getElementById("card-six");
-const cardSeven = document.getElementById("card-seven");
-const cardEight = document.getElementById("card-eight");
-const cardNine = document.getElementById("card-nine");
-const cardTen = document.getElementById("card-ten");
-const cardEleven = document.getElementById("card-eleven");
-const cardTwelve = document.getElementById("card-twelve");
-
-const duplicateCheckArray = [cardOne.src, cardTwo.src, cardThree.src, cardFour.src, cardFive.src, cardSix.src, cardSeven.src, cardEight.src, cardNine.src, cardTen.src, cardEleven.src, cardTwelve.src];
-console.log(duplicateCheckArray);
 const cardStorageArray = [{}];
-console.log(cardStorageArray);
-const backSrc = "./cards/cardBack.png";
+const backSrc = "./cards/cardBack.webp";
 
 function toggleCard(event) {
   const card = event.target;
