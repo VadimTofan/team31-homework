bbab6b7a9c0f471f3660d21074a5b65a8784f089
Vadim Tofan <vad.tofan@gmail.com>
2025-03-03 01:57:08 +0100

    Add multiple card images for Homework project and update .gitignore

---
+++ javascript/javascript2/week1/Homework/index.js
@@ -0,0 +1,91 @@
+const cardSelector = document.querySelectorAll("#card-one, #card-two, #card-three, #card-four, #card-five, #card-six, #card-seven, #card-eight, #card-nine, #card-ten, #card-eleven, #card-twelve");
+
+const cardsArray = [
+  { cardName: "Ace of Spades", cardUrl: "./cards/AS.jpg" },
+  { cardName: "Two of Spades", cardUrl: "./cards/2S.jpg" },
+  { cardName: "Three of Spades", cardUrl: "./cards/3S.jpg" },
+  { cardName: "Four of Spades", cardUrl: "./cards/4S.jpg" },
+  { cardName: "Five of Spades", cardUrl: "./cards/5S.jpg" },
+  { cardName: "Six of Spades", cardUrl: "./cards/6S.jpg" },
+  { cardName: "Seven of Spades", cardUrl: "./cards/7S.jpg" },
+  { cardName: "Eight of Spades", cardUrl: "./cards/8S.jpg" },
+  { cardName: "Nine of Spades", cardUrl: "./cards/9S.jpg" },
+  { cardName: "Ten of Spades", cardUrl: "./cards/10S.jpg" },
+  { cardName: "Jack of Spades", cardUrl: "./cards/JS.jpg" },
+  { cardName: "Queen of Spades", cardUrl: "./cards/QS.jpg" },
+  { cardName: "King of Spades", cardUrl: "./cards/KS.jpg" },
+
+  { cardName: "Ace of Hearts", cardUrl: "./cards/AH.jpg" },
+  { cardName: "Two of Hearts", cardUrl: "./cards/2H.jpg" },
+  { cardName: "Three of Hearts", cardUrl: "./cards/3H.jpg" },
+  { cardName: "Four of Hearts", cardUrl: "./cards/4H.jpg" },
+  { cardName: "Five of Hearts", cardUrl: "./cards/5H.jpg" },
+  { cardName: "Six of Hearts", cardUrl: "./cards/6H.jpg" },
+  { cardName: "Seven of Hearts", cardUrl: "./cards/7H.jpg" },
+  { cardName: "Eight of Hearts", cardUrl: "./cards/8H.jpg" },
+  { cardName: "Nine of Hearts", cardUrl: "./cards/9H.jpg" },
+  { cardName: "Ten of Hearts", cardUrl: "./cards/10H.jpg" },
+  { cardName: "Jack of Hearts", cardUrl: "./cards/JH.jpg" },
+  { cardName: "Queen of Hearts", cardUrl: "./cards/QH.jpg" },
+  { cardName: "King of Hearts", cardUrl: "./cards/KH.jpg" },
+
+  { cardName: "Ace of Diamonds", cardUrl: "./cards/AD.jpg" },
+  { cardName: "Two of Diamonds", cardUrl: "./cards/2D.jpg" },
+  { cardName: "Three of Diamonds", cardUrl: "./cards/3D.jpg" },
+  { cardName: "Four of Diamonds", cardUrl: "./cards/4D.jpg" },
+  { cardName: "Five of Diamonds", cardUrl: "./cards/5D.jpg" },
+  { cardName: "Six of Diamonds", cardUrl: "./cards/6D.jpg" },
+  { cardName: "Seven of Diamonds", cardUrl: "./cards/7D.jpg" },
+  { cardName: "Eight of Diamonds", cardUrl: "./cards/8D.jpg" },
+  { cardName: "Nine of Diamonds", cardUrl: "./cards/9D.jpg" },
+  { cardName: "Ten of Diamonds", cardUrl: "./cards/10D.jpg" },
+  { cardName: "Jack of Diamonds", cardUrl: "./cards/JD.jpg" },
+  { cardName: "Queen of Diamonds", cardUrl: "./cards/QD.jpg" },
+  { cardName: "King of Diamonds", cardUrl: "./cards/KD.jpg" },
+
+  { cardName: "Ace of Clubs", cardUrl: "./cards/AC.jpg" },
+  { cardName: "Two of Clubs", cardUrl: "./cards/2C.jpg" },
+  { cardName: "Three of Clubs", cardUrl: "./cards/3C.jpg" },
+  { cardName: "Four of Clubs", cardUrl: "./cards/4C.jpg" },
+  { cardName: "Five of Clubs", cardUrl: "./cards/5C.jpg" },
+  { cardName: "Six of Clubs", cardUrl: "./cards/6C.jpg" },
+  { cardName: "Seven of Clubs", cardUrl: "./cards/7C.jpg" },
+  { cardName: "Eight of Clubs", cardUrl: "./cards/8C.jpg" },
+  { cardName: "Nine of Clubs", cardUrl: "./cards/9C.jpg" },
+  { cardName: "Ten of Clubs", cardUrl: "./cards/10C.jpg" },
+  { cardName: "Jack of Clubs", cardUrl: "./cards/JC.jpg" },
+  { cardName: "Queen of Clubs", cardUrl: "./cards/QC.jpg" },
+  { cardName: "King of Clubs", cardUrl: "./cards/KC.jpg" },
+];
+
+const backSrc = "./cards/cardBack.png";
+function toggleCard(event) {
+  const card = event.target;
+  const cardSrc = card.src.split("/").pop();
+  const random = Math.floor(Math.random() * (sixCards.length + 1));
+  if (cardSrc.includes("cardBack")) {
+    card.src = sixCards[random].cardUrl;
+    card.alt = `${sixCards[random].cardName}`;
+    sixCards.splice(random, 1);
+  } else {
+    card.src = backSrc;
+  }
+}
+
+cardSelector.forEach((card) => {
+  card.addEventListener("click", toggleCard);
+});
+
+const sixCards = [];
+function generateRandomCard() {
+  if (sixCards.length === 0) {
+    for (let i = 0; i < 6; i++) {
+      const randomNumber = Math.floor(Math.random() * cardsArray.length);
+      sixCards.push(cardsArray[randomNumber]);
+      sixCards.push(cardsArray[randomNumber]);
+    }
+  }
+  return;
+}
+generateRandomCard();
+console.log(sixCards);
